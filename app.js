7b65581d9fd1458fcd6ade5c64e87166bd801366
supuna97 <supunnanayakkara01@gmail.com>
2023-10-08 23:02:19 +0530

    Add cors and body-parser

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const passport = require("passport");
 const session = require("express-session");
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const authenticateToken = require("./src/middleware/tokenAuthentication");
 const authRoutes = require("./src/routes/auth");
@@ -9,7 +11,11 @@ const apiRoutes = require("./src/routes/api-v1");
 const app = express();
 const port = process.env.PORT || 3000;
 
+//middlewares
+app.use(bodyParser.json());
 app.use(express.json());
+app.use(cors()); // Allow all origins (not recommended for production)
+
 app.use(
   session({
     secret: process.env.APP_SECRET,
