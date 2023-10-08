e28f706d683c99c6d186cab3e240d5df0169e9f2
supuna97 <supunnanayakkara01@gmail.com>
2023-10-08 21:51:24 +0530

    Create middleware for the token auth.

---
+++ src/middleware/token-authentication.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const authenticateToken = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: "Authentication required" });
+  }
+
+  jwt.verify(token, process.env.APP_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = authenticateToken;
